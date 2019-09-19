fce626e72313f408207a1e2805fae18bff41b626
Michael Bell <michael.bell@mdckc.com>
2019-09-19 00:48:16 -0500

    Commit 3

--- EmployeeSQL/Homework09.sql
+++ EmployeeSQL/Homework09.sql
@@ -48,6 +48,7 @@ create table titles (
 );
 
 -- Data Analysis
+-- One
 select employees.emp_no, 
 	employees.last_name, 
 	employees.first_name, 
@@ -57,6 +58,7 @@ from employees
 join salaries
 on employees.emp_no = salaries.emp_no;
 
+-- Two
 select employees.emp_no, 
 	employees.last_name, 
 	employees.first_name, 
@@ -68,6 +70,7 @@ join salaries
 on employees.emp_no = salaries.emp_no
 where employees.hire_date between '1986-01-01' and '1986-12-31';
 
+--Three
 select departments.dept_no,
 	departments.dept_name,
 	dept_manager.emp_no,
@@ -81,7 +84,8 @@ join departments
 on dept_manager.dept_no = departments.dept_no
 	join employees
 	on dept_manager.emp_no = employees.emp_no;
-	
+
+--Four
 select employees.emp_no,
 	employees.last_name, 
 	employees.first_name, 
@@ -91,4 +95,44 @@ join departments
 on dept_emp.dept_no = departments.dept_no
 	join employees
 	on dept_emp.emp_no = employees.emp_no;
+
+--Five
+select employees.emp_no, 
+	employees.last_name, 
+	employees.first_name
+from employees
+where employees.last_name like 'B%' and employees.first_name = 'Hercules'
+order by employees.last_name;
+
+--Six
+select employees.emp_no,
+	employees.last_name, 
+	employees.first_name, 
+	departments.dept_name
+from dept_emp
+join departments
+on dept_emp.dept_no = departments.dept_no
+	join employees
+	on dept_emp.emp_no = employees.emp_no
+where departments.dept_name = 'Sales'
+order by (employees.last_name, employees.first_name);
+
+--Seven
+select employees.emp_no,
+	employees.last_name, 
+	employees.first_name, 
+	departments.dept_name
+from dept_emp
+join departments
+on dept_emp.dept_no = departments.dept_no
+	join employees
+	on dept_emp.emp_no = employees.emp_no
+where departments.dept_name = 'Sales' or departments.dept_name = 'Development'
+order by (employees.last_name, employees.first_name);
+
+--Eight
+select employees.last_name, count(employees.last_name)
+from employees
+group by employees.last_name
+order by employees.last_name desc
 	
